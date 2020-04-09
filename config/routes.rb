40227bcb25a34e0818e1093b02b8942e2468c02d
HumzaBaig <humzacodemail@gmail.com>
2020-04-09 03:29:15 -0500

    add routes/pages/links for terms, cookie policy, and privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :john_gillons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#index'
 
   # sitemap
   match '/sitemap', to: 'static#sitemap', via: 'get', as: :sitemap
+
+  # policy pages
+  match '/terms-conditions', to: 'static#terms_conditions', via: 'get', as: :terms_conditions
+  match '/privacy-policy', to: 'static#privacy_policy', via: 'get', as: :privacy_policy
+  match '/cookie-policy', to: 'static#cookie_policy', via: 'get', as: :cookie_policy
 
   # influencer landing pages
   get '/brianda', to: 'briandas#new'
